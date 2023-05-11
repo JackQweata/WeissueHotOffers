c209017d3bfcd9793dffa930d52a88c85c963f1d
JackQweata <dahil3@yandex.ru>
2023-05-12 00:04:31 +0300

    добавлена логгирование

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-from src.WB.module.product_card import CardProduct
+from decouple import config
+from src.WB.run_parser import start_parse
+from src.bots.admin.admin_commads import commands_admin_bot
+from utils.run_threading import start_threading
 
-CardProduct(21154521).get_image()
+
+def main():
+    active_processes = [
+        # {"target": start_parse, "args": ((config("WB_URL_WOMAN_CLOTHES")), ("woman"), )},
+        {"target": start_parse, "args": ((config("WB_URL_MEN_CLOTHES")), ("men"), )},
+        # {"target": start_parse, "args": ((config("WB_URL_CHILDREN_CLOTHES")), "children",)},
+        {"target": commands_admin_bot, "args": ()}
+
+    ]
+
+    start_threading(active_processes)
+
+
+if __name__ == '__main__':
+    main()
--- utils/api_request.py
+++ utils/api_request.py
@@ -4,8 +4,9 @@ from fake_headers import Headers
 
 def response(url):
     header = Headers(headers=True)
-
     try:
+
         return requests.get(url, headers=header.generate()).json()
+
     except Exception as err:
         return
